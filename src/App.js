8e1e60cce5571897e626b4b910381121ea24f581
Denlavor <70455070+Rafa-Denlavor@users.noreply.github.com>
2022-11-11 14:52:54 +0000

    feat: create Gauge component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './global.scss';
+import Gauge from './components/Gauge';
+
+function App() {
+  return (
+    <div>
+      <Gauge meaning='neutral' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gauge/index.js
@@ -0,0 +1,20 @@
+import "./Gauge.scss";
+import cx from "classnames";
+import propTypes from 'prop-types';
+
+function Gauge(props) {
+  return (
+    <div className="gaugeWrapper">
+      <div className={cx('halfCircle', props.meaning)}>
+        <p className="score">560</p>
+        <p className="label">{'/1000'}</p>
+        </div>
+    </div>
+  );
+}
+
+Gauge.propTypes = {
+  meaning: propTypes.oneOf(['good', 'warn', 'bad']).isRequired,
+};
+
+export default Gauge;
